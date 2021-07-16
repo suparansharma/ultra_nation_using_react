2d6ae6cb9405de5c0339cdb5d30b3b22d08aa7f2
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-16 15:54:55 +0600

    take url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [countries,setCountries]=useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res=>res.json())
+    .then(data=>{
+      setCountries(data)
+      console.log(data)
+      const names = data.map(country=> country.name);
+    })
+    .catch(error=>console.log(error))
+  },[])
   return (
     <div className="App">
       <header className="App-header">
+        <h1>County : {countries.length}</h1>
+        <ul>
+          {
+            countries.map(country=> <li>{country.name}</li>)
+          }
+        </ul>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
